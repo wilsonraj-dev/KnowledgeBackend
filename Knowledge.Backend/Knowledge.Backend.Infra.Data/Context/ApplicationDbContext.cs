9f54b6b6f86f03e3969ffb0a968d8921cba872ae
Wilson Junior <wilson.assis.junior@gmail.com>
2024-02-29 20:07:47 -0300

    Creating DbContext for entities

---
+++ Knowledge.Backend/Knowledge.Backend.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Knowledge.Backend.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Knowledge.Backend.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
